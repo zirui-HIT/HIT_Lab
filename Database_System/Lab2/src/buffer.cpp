6e4c12b8e5cd57912d73231064cb05e18f96cf5b
WDZRMPCBIT <WDZRMPCBIT@163.com>
2021-04-19 11:09:05 +0800

    add code waiting to be implemented

---
+++ Database_System/Lab2/src/buffer.cpp
@@ -0,0 +1,91 @@
+/**
+ * @author See Contributors.txt for code contributors and overview of BadgerDB.
+ *
+ * @section LICENSE
+ * Copyright (c) 2012 Database Group, Computer Sciences Department, University of Wisconsin-Madison.
+ */
+
+#include <memory>
+#include <iostream>
+#include "buffer.h"
+#include "exceptions/buffer_exceeded_exception.h"
+#include "exceptions/page_not_pinned_exception.h"
+#include "exceptions/page_pinned_exception.h"
+#include "exceptions/bad_buffer_exception.h"
+#include "exceptions/hash_not_found_exception.h"
+
+namespace badgerdb { 
+
+BufMgr::BufMgr(std::uint32_t bufs)
+	: numBufs(bufs) {
+	bufDescTable = new BufDesc[bufs];
+
+  for (FrameId i = 0; i < bufs; i++) 
+  {
+  	bufDescTable[i].frameNo = i;
+  	bufDescTable[i].valid = false;
+  }
+
+  bufPool = new Page[bufs];
+
+	int htsize = ((((int) (bufs * 1.2))*2)/2)+1;
+  hashTable = new BufHashTbl (htsize);  // allocate the buffer hash table
+
+  clockHand = bufs - 1;
+}
+
+
+BufMgr::~BufMgr() {
+}
+
+void BufMgr::advanceClock()
+{
+
+}
+
+void BufMgr::allocBuf(FrameId & frame) 
+{
+}
+
+	
+void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
+{
+}
+
+
+void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
+{
+}
+
+void BufMgr::flushFile(const File* file) 
+{
+}
+
+void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
+{
+}
+
+void BufMgr::disposePage(File* file, const PageId PageNo)
+{
+    
+}
+
+void BufMgr::printSelf(void) 
+{
+  BufDesc* tmpbuf;
+	int validFrames = 0;
+  
+  for (std::uint32_t i = 0; i < numBufs; i++)
+	{
+  	tmpbuf = &(bufDescTable[i]);
+		std::cout << "FrameNo:" << i << " ";
+		tmpbuf->Print();
+
+  	if (tmpbuf->valid == true)
+    	validFrames++;
+  }
+
+	std::cout << "Total Number of Valid Frames:" << validFrames << "\n";
+}
+
+}
